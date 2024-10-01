aacc1c0a28552510400cd7d1bef8cc3b9234c392
Mihaela Talmazan <talmazanmihaela@gmail.com>
2024-10-01 09:51:18 +0200

    Added dark and light theme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import "./App.css";
+import { useState } from "react";
+import styled from "@emotion/styled";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const StyledButton = styled("button")(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.common.white,
+    border: "none",
+    borderRadius: "4px",
+    padding: "10px 20px",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  }));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <CssBaseline />
+        <main>{darkMode ? "DARK MODE" : "LIGHT MODE"}</main>
+        <StyledButton onClick={handleThemeChange}>Change Theme</StyledButton>
+      </ThemeProvider>
     </div>
   );
 }
